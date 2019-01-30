cee3042f9b3c6df31d988f4011ea05ad4dfe69fc
Tiffany Carter <tafby88@gmail.com>
2019-01-30 01:22:51 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,26 @@
 class Book
   attr_accessor :author, :page_count, :genre
+<<<<<<< HEAD
+=======
+
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+
+
+
+
+end # end of the book class
+>>>>>>> 7b95c06ec03ae584a60e8a9d2da785647cd4c59a
 
   def initialize(title)
     @title = title
